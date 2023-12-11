6885f109be31c457b26b1cff123bb0eacd711e7c
gayashan <gayashan@ebipl.com>
2023-12-11 10:06:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [userInput, setUserInput] = useState('');
+
+  const sendMessage = async () => {
+    if (!userInput.trim()) return;
+
+    const userMessage = { role: 'user', content: userInput };
+    setMessages([...messages, userMessage]);
+
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/chat`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: userInput }),
+    });
+
+    const { response: botMessage } = await response.json();
+    setMessages([...messages, userMessage, { role: 'bot', content: botMessage }]);
+    setUserInput('');
+  };
+
+  const handleUserInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleUserInputSubmit = (e) => {
+    e.preventDefault();
+    sendMessage();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chat with GPT-4</h1>
       </header>
+      <main>
+        <div className="message-container">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.role}`}>
+              {message.content}
+            </div>
+          ))}
+        </div>
+        <form onSubmit={handleUserInputSubmit}>
+          <input
+            type="text"
+            value={userInput}
+            onChange={handleUserInputChange}
+            placeholder="Say something..."
+          />
+          <button type="submit">Send</button>
+        </form>
+      </main>
     </div>
   );
 }
